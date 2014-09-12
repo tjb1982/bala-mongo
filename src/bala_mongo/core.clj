36f84f522a355fcce336073593bf6c477253abdb
Tom Brennan <tjb1982@gmail.com>
2014-09-12 14:08:58 -0400

    better reporting output

--- src/bala_mongo/core.clj
+++ src/bala_mongo/core.clj
@@ -3,77 +3,132 @@
   (:import (org.apache.commons.lang3 ArrayUtils))
   (:gen-class))
 
-(defn as-bytes
-  [buf]
-  (apply str (for [b (.toByteArray buf)] (format "%02x " b))))
+(def sep "|||")
 
-(def msg-header (m/struct :message-length m/sint32
-                          :request-id m/sint32
-                          :response-to m/sint32
-                          :op-code m/sint32))
+(def file-name
+  (str "bala-mongo-output "
+       (-> (java.util.Date.) .toString)
+       ".csv"))
 
-(def op-reply (m/struct :header msg-header
-                        :response-flags m/sint32
-                        :cursor-id m/sint64
-                        :starting-from m/sint32
-                        :number-returned m/sint32
-                        :first-document-length m/sint32))
+(defprotocol ByteFormat
+  (as-hex [bytez]))
 
-(def op-query (m/struct :header msg-header
-                        :flags m/sint32
-;;                        :full-collection-name m/ascii-string
-                        :number-to-skip m/sint32
-                        :number-to-return m/sint32
-                        :query-document-length m/sint32))
+(extend-protocol ByteFormat
+  
+  (Class/forName "[B")
+  (as-hex [bytez]
+    (apply str (for [b bytez] (format "%02x " b))))
+  
+  java.io.ByteArrayOutputStream
+  (as-hex [bytez]
+    (as-hex (.toByteArray bytez)))
 
-(defn parse-message
-  [buf & more]
-  (let [header (m/read (java.io.ByteArrayInputStream.
-			 (.toByteArray buf) 0 16)
-		       msg-header)]
-;    (try 
-    (m/read (java.io.ByteArrayInputStream. (.toByteArray buf))
-      (condp = (-> header :op-code)
-        1 op-reply
-        1000 "OP_MSG"
-        2001 "OP_UPDATE"
-        2002 "OP_INSERT"
-        2003 "RESERVED"
-        2004 op-query
-        2005 "OP_GET_MORE"
-        2006 "OP_DELETE"
-        2007 "OP_KILL_CURSORS"))
-;       (catch Exception e {:e (.getMessage e)}))
-    ))
+  java.nio.ByteBuffer
+  (as-hex [bytez]
+    (as-hex (.array bytez))))
 
-(defn report-message
+(defn lil-byte-buffer
+  [byte-arr]
+  (-> (java.nio.ByteBuffer/wrap byte-arr)
+    (.order java.nio.ByteOrder/LITTLE_ENDIAN)))
+
+(defn get-c-string
+  ([bb] (get-c-string bb ""))
+  ([bb string]
+    (when (.hasRemaining bb)
+      (let [b (.get bb)]
+        (if (not= b 0)
+          (get-c-string bb (str string (char b)))
+	  string)))))
+
+(defn parse-header
+  [bb]
+  (if (.hasRemaining bb)
+    {:message-length (.getInt bb)
+     :request-id (.getInt bb)
+     :response-to (.getInt bb)
+     :op-code (.getInt bb)}))
+
+(defn parse-documents
+  ([bb] (parse-documents bb -1))
+  ([bb remaining]
+    (parse-documents bb remaining nil))
+  ([bb remaining documents]
+    (if (or (> remaining 0)
+            (and (< remaining 0)
+                 (.hasRemaining bb)))
+      (let [doc-length (.getInt bb)]
+        (.position bb (- (.position bb) 4))
+        (let [documents (conj documents
+                          (let [ba (byte-array doc-length)]
+                            (.get bb ba 0 doc-length)
+                            (com.mongodb.util.JSON/serialize
+			      (org.bson.BSON/decode ba))))]
+	  (parse-documents bb (dec remaining) documents)))
+      documents)))
+
+(defn parse-reply
+  [bb]
+  (let [body {:response-flags (.getInt bb)
+              :cursor-id (.getLong bb)
+              :starting-from (.getInt bb)
+              :number-returned (.getInt bb)}]
+    (assoc body :documents
+      (parse-documents bb (-> body :number-returned)))))
+
+(defn parse-query
+  [bb]
+  (let [body {:flags (.getInt bb)
+              :full-collection-name (get-c-string bb)
+              :number-to-skip (.getInt bb)
+              :number-to-return (.getInt bb)
+	      :query (parse-documents bb)}]
+    body))
+
+(defn parse-insert
+  [bb]
+  (let [body {:flags (.getInt bb)
+              :full-collection-name (get-c-string bb)
+              :documents (parse-documents bb)}]
+    body))
+
+(defn parse-msg
+  [msg]
+  (let [bb (lil-byte-buffer (.toByteArray msg))
+        header (parse-header bb)]
+    (if (.hasRemaining bb)
+      {:header header
+       :body (condp = (-> header :op-code)
+               1 (parse-reply bb)
+               2002 (parse-insert bb)
+               2004 (parse-query bb)
+               nil)})))
+
+(defn report-responses
   [pkg]
-  (let [sep "|||"]
-    (with-open [f (clojure.java.io/writer "bala-mongo-output.csv" :append true)]
-      (doseq [response (-> pkg :responses)]
-        #_(println (parse-message (-> pkg :request))
-                 (parse-message (-> response :buffer))
-                 "\n")))))
+  (doall
+    (map-indexed
+      (fn [i resp]
+        (with-open [w (clojure.java.io/writer
+                        file-name :append true)]
+          (.write w
+	    (str
+	      (clojure.string/join sep
+                [(-> resp :server :name)
+                 (str (-> resp :server :host) ":" (-> resp :server :port))
+                 (as-hex (-> pkg :request))
+                 (parse-msg (-> pkg :request))
+                 (as-hex (-> resp :buffer))
+                 (parse-msg (-> resp :buffer))])
+            "\r\n"))))
+      (-> pkg :responses))))
 
 (defn read-wire
   [in]
   (let [header-bytes (byte-array 16)
         first-read (.read in header-bytes 0 16)]
     (if (not= -1 first-read)
-      (let [header (m/read (java.io.ByteArrayInputStream. header-bytes) msg-header)
-            leftover-bytes (byte-array (- (:message-length header) 16))]
-        (if-let [second-read (not= -1 (.read in leftover-bytes 0 (- (:message-length header) 16)))]
-          (ArrayUtils/addAll header-bytes leftover-bytes)
-          -1))
-      first-read)))
-
-(defn read-wire
-  [in]
-  (let [header-bytes (byte-array 16)
-        first-read (.read in header-bytes 0 16)]
-    (if (not= -1 first-read)
-      (let [leftover (- (-> (java.nio.ByteBuffer/wrap header-bytes)
-                          (.order java.nio.ByteOrder/LITTLE_ENDIAN)
+      (let [leftover (- (-> (lil-byte-buffer header-bytes)
                           .getInt)
                         16)
             leftover-bytes (byte-array leftover)]
@@ -81,6 +136,20 @@
           (ArrayUtils/addAll header-bytes leftover-bytes)
           -1))
       first-read)))
+
+(defn before-create-server
+  [props]
+  (with-open [w (clojure.java.io/writer
+                  file-name)]
+    (.write w
+      (str (clojure.string/join sep
+        ["Server Name"
+         "Address"
+         "Raw Request"
+         "Request"
+         "Raw Response"
+         "Response"])
+      "\r\n"))))
 
 (defn read-request
   [in]
@@ -92,5 +161,5 @@
 
 (defn handle-interchange
   [pkg]
-  (report-message pkg))
+  (report-responses pkg))
 
